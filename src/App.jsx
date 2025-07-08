6bb39dd9c470b5a4f41dc7959d36d4f2f64805c3
ignnaaroans95 <1995igna@gmail.com>
2025-07-08 13:04:43 +0200

    Subida inicial completa del proyecto: estructura, frontend, estilos, fuentes y assets

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Amstrong', fontSize: '40px', color: 'black', backgroundColor: 'gray', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px' }}>
+      Fuente Amstrong cargada
+    </div>
+  );
+}
+
+
+export default App
